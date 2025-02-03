08da7a9125842fca1a2d8ddd71db562911e3f5d5
Rohan Bhapkar <127857176+ro-nav@users.noreply.github.com>
2025-02-03 13:48:55 +0530

    Removed unnecessary imports in UserTokenRepository

--- Backend/P04Auth/src/main/java/com/tradingapp/P04Auth/repositories/UserTokenRepository.java
+++ Backend/P04Auth/src/main/java/com/tradingapp/P04Auth/repositories/UserTokenRepository.java
@@ -3,11 +3,11 @@ package com.tradingapp.P04Auth.repositories;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.tradingapp.P04Auth.entities.UserToken;
 
-import jakarta.transaction.Transactional;
-
+@Repository
 public interface UserTokenRepository extends JpaRepository<UserToken, Integer> {
 	Optional<UserToken> findByClientCode(String clientCode);
 
