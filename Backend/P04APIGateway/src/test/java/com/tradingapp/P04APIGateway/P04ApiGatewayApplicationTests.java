b15071ce9c0182259ffc5f24b93fee204856f391
Rohan Bhapkar <127857176+ro-nav@users.noreply.github.com>
2025-02-10 00:16:02 +0530

    feat: Add API Gateway for microservices communication and routing

---
+++ Backend/P04APIGateway/src/test/java/com/tradingapp/P04APIGateway/P04ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tradingapp.P04APIGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P04ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
